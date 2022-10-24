fa93b0e783c30357b984703e0688cac4f33c2806
self479 <yiSiNing479@163.com>
2022-10-24 23:18:01 +0800

    通过序列化对数据处理的类

---
+++ src/main/java/com/zhou/mapreduce/Writable/FlowBean.java
@@ -0,0 +1,69 @@
+package com.zhou.mapreduce.Writable;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlowBean implements Writable {
+    private long upFlow; //上行流量
+    private long downFlow; //下行流量
+    private long sumFlow; //总流量
+
+    //空参构造
+    public FlowBean() {
+    }
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    public void setSumFlow() {
+        this.sumFlow = this.upFlow + this.downFlow;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeLong(upFlow);
+        out.writeLong(downFlow);
+        out.writeLong(sumFlow);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        this.upFlow = in.readLong();
+        this.downFlow = in.readLong();
+        this.sumFlow = in.readLong();
+    }
+
+    /*
+     * 希望展示的格式为:
+     *  1116     854     2070
+     *  上行流量   下行流量 总流量
+     *  */
+    @Override
+    public String toString() {
+        return upFlow + "\t" + downFlow + "\t" + sumFlow;
+    }
+}
