63d83f6dded42fc805034e5eb35f4207396c55c7
self479 <yiSiNing479@163.com>
2022-10-25 16:59:42 +0800

    将inputflow中的文件进行处理并计算, 最终得出每个手机号的上下行总流量的结果并输出到文件

---
+++ src/main/java/com/zhou/mapreduce/Writable/FlowDriver.java
@@ -0,0 +1,36 @@
+package com.zhou.mapreduce.Writable;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class FlowDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        // 1 获取job
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance();
+        // 2 设置jar包路径
+        job.setJarByClass(FlowDriver.class);
+        // 3 关联mapper和reducer
+        job.setMapperClass(FlowMapper.class);
+        job.setReducerClass(FlowReducer.class);
+        // 4 设置map输出的KV类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(FlowBean.class);
+        // 5 设置最终输出的KV类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+        // 6 路径
+        FileInputFormat.setInputPaths(job, new Path("D:\\projects\\IDEworkspace\\MapReduceDemo\\inputflow\\phone_data.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("D:\\projects\\IDEworkspace\\MapReduceDemo\\outputflow"));
+        // 7 提交job
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : 1);
+    }
+}
