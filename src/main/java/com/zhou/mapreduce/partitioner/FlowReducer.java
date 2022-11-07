80d4218f44fec177bc6fe7aab8c62e36796847b2
self479 <yiSiNing479@163.com>
2022-11-07 20:07:03 +0800

    自定义分区案例, 在Writable模块基础上写的, 目的是将不同数字开头的手机号的流量信息输出结果到不同文件

---
+++ src/main/java/com/zhou/mapreduce/partitioner/FlowReducer.java
@@ -0,0 +1,33 @@
+package com.zhou.mapreduce.partitioner;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class FlowReducer extends Reducer<Text, FlowBean, Text, FlowBean> {
+    private FlowBean outV = new FlowBean();
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values, Context context) throws IOException, InterruptedException{
+        //Text key:手机号
+        //Iterable<FlowBean> values:相同的key(手机号)的各自对应的FlowBean对象(FlowBean对象中包括上下行总流量数据)
+        //一个手机号进入一次该函数
+        // 1 遍历集合累加值
+        long totalUp = 0;
+        long totalDown = 0;
+
+        // 2 将相同手机号的上行和下行流量相加
+        for(FlowBean value : values){
+            totalUp += value.getUpFlow();
+            totalDown += value.getDownFlow();
+        }
+
+        // 3 封装outK, outV
+        outV.setUpFlow(totalUp);
+        outV.setDownFlow(totalDown);
+        outV.setSumFlow();
+
+        // 4 写出
+        context.write(key,outV);
+    }
+}
