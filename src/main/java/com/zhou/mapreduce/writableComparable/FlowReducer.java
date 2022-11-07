2c92bfc07814e15aedef51451c8acb1c9d23ca92
self479 <yiSiNing479@163.com>
2022-11-07 21:49:28 +0800

    以序列化案例writable为基础, 并且以writable案例的输出作为此案例的输入, 输出的数据按总流量的大小倒序排序

---
+++ src/main/java/com/zhou/mapreduce/writableComparable/FlowReducer.java
@@ -0,0 +1,17 @@
+package com.zhou.mapreduce.writableComparable;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class FlowReducer extends Reducer<FlowBean, Text, Text, FlowBean> {
+
+    @Override
+    protected void reduce(FlowBean key, Iterable<Text> values, Reducer<FlowBean, Text, Text, FlowBean>.Context context) throws IOException, InterruptedException {
+        // values中是多个总流量(key)相同的手机号(整合的效果), 为了要得到所有手机号的数据, 因此要逐个遍历写出
+        for (Text value : values){
+            context.write(value, key);
+        }
+    }
+}
